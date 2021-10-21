976b9e2c2ba8c55741ce178e9188411eec12edf5
Mace Ojala <mace.ojala@gmail.com>
2021-10-21 21:34:26 +0700

    Refactor to drop the preview mp3 in /tmp and render from there.

--- freesoundoftheday.lua
+++ freesoundoftheday.lua
@@ -45,17 +45,19 @@ function get_sound_metadata(sound_id)
 end
 
 function get_sound_preview(metadata)
-   if debug then print(":cd: Downloading preview for "..metadata['name']) end
-   local filename = create_filename(metadata)
-   download_file(metadata['previews']['preview-hq-mp3'], filename)
-   return filename
+   if debug then print("💿 Downloading preview for "..metadata['name']) end
+   local filename = extract_preview_filename(metadata)
+   local complete_path = "/tmp/"..filename
+   download_file(metadata['previews']['preview-hq-mp3'], complete_path)
+   return complete_path
 end
 
 --------------- lower level network connections -------
 
 function download_file(url, filename)
    if debug then print("📡 Downloading file from "..url) end
-   io.popen("curl -s "..url.. " -o "..DIRECTORY..filename)
+   -- io.popen("curl -s "..url.. " -o "..DIRECTORY..filename)
+   io.popen("curl -s "..url.." -o "..filename)
 end
 
 function query_api(resource, param)
@@ -63,11 +65,56 @@ function query_api(resource, param)
    local query_url = API_ENDPOINT..resource.."/"..param.."/"
    local auth_url = query_url.."?token="..API_TOKEN
    if debug then print("🛂 "..auth_url) end
-   local response = assert(io.popen("curl -s "..auth_url))
+   local response = assert(io.popen("curl -s "..auth_url..";sync"))
    local content = response:read '*all'
    response:close()
    -- if debug then print("📇 Read metadata "..content) end
    return content
+end
+
+----------------- manage files ---------------
+
+function some_sounds_already_on_disk(directory)
+   local files = {}
+   for filename in lfs.dir(directory) do
+      if filename ~= "." and filename ~= ".." then
+         table.insert(files, filename)
+      end
+   end
+   return (#files > 0)
+end
+
+function sound_already_on_disk(directory, metadata)
+   local filename_would_be = create_richer_preview_filename(metadata)
+   if debug then print("🗃 Checking for file "..filename_would_be
+                       .." in directory "..directory) end
+   local files = {}
+   for filename in lfs.dir(directory) do
+      if (filename == filename_would_be) then return true end
+   end
+   return false
+end
+
+function purge_all_except(directory, metadata)
+   local keeper = create_richer_preview_filename(metadata)
+   for filename in lfs.dir(directory) do
+      if filename ~= "." and filename ~= ".." then
+         if filename ~= keeper then
+            if debug then print("🗑 Binning "..filename) end
+            io.popen("rm "..directory.."/"..filename)
+         else
+            if debug then print("♻ Would keep "..filename) end
+         end
+      end
+   end
+end
+
+function render_to_wav(fromfile, tofile)
+   -- Because OAuth2 is hard lol.
+   if debug then print("🔊 Rendering file "..fromfile) end
+   local wavfilename = tofile:gsub('.mp3$', '.wav')
+   if debug then print("🔊 Rendering into "..wavfilename) end
+   io.popen("nice ffmpeg -n -loglevel warning -i "..fromfile.." "..wavfilename)
 end
 
 ----------------- utilities -----------------
@@ -76,64 +123,39 @@ function parse_metadata(metadata)
    return lunajson.decode(metadata)
 end
 
-function create_filename(metadata)
+function create_richer_preview_filename(metadata)
    local filename = metadata['id']
       .."-"..metadata['name'].."-by-"..metadata['username']
-      ..'.mp3'
+      ..'.wav'
    return filename
 end
 
-function some_sounds_already_on_disk()
-   local files = {}
-   for filename in lfs.dir(DIRECTORY) do
-      if filename ~= "." and filename ~= ".." then
-         table.insert(files, filename)
-      end
-   end
-   return (#files ~= 0)
+function extract_preview_filename(metadata)
+   local filename = metadata['previews']['preview-hq-mp3']:gsub('^.*/', '')
+   if debug then print("🗜 preview filename is "..filename) end
+   return filename
 end
-
-function sound_already_on_disk(metadata)
-   local filename_would_be = create_filename(metadata)
-   if debug then print("🗃 Checking for file "..filename_would_be
-                       .." in directory "..DIRECTORY) end
-
-   local files = {}
-   for filename in lfs.dir(DIRECTORY) do
-      if (filename == filename_would_be) then return true end
-   end
-   return false
-end
-
------- audio processing ----- because OAuth2 is hard lol ------
-
-function render_to_wav(filename)
-   local complete_path = DIRECTORY..filename
-   if debug then print("🔊 Rendering file "..complete_path) end
-   complete_path_wav = string.gsub(complete_path, "[^.]+$", "wav")
-   if debug then print("🔊 Rendering into "..complete_path_wav) end
-   io.popen("nice ffmpeg -n -loglevel warning -i "..complete_path.." "..complete_path_wav)
-end
-
 
 ---------- main -----------
 
 function main()
    -- local fp = get_frontpage('frontpage.html')
-
    local fp = get_frontpage()
    local sound_id = get_featured_sound_id(fp)
    local sound_of_the_day = get_sound_metadata(sound_id)
    print("Today's random sound of the day is "
          ..sound_of_the_day['name'].." by "..sound_of_the_day['username'])
 
-   if (not sound_already_on_disk(sound_of_the_day)) then
-      if debug then print("▶ Getting random sound of the day.") end
-      preview_filename = get_sound_preview(sound_of_the_day)
-      render_to_wav(preview_filename)
+   if sound_already_on_disk(DIRECTORY, sound_of_the_day) then
+      if debug then print("⏹ Already got "..sound_of_the_day['name']) end
    else
-      if debug then print("⏹ But its today's "..sound_of_the_day['name']) end
+      if debug then print("▶ Getting random sound of the day.") end
+      local tmp_preview_filename = get_sound_preview(sound_of_the_day)
+      local rendered_preview_filename = DIRECTORY
+         ..create_richer_preview_filename(sound_of_the_day)
+      render_to_wav(tmp_preview_filename, rendered_preview_filename)
    end
+   purge_all_except(DIRECTORY, sound_of_the_day)
 end
 
 ---------------------- run things -------------
